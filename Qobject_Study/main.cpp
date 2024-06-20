754ea79fb2686b155bd250d56c38b907aa2a04f3
venkatesh <sonicspeedw@gmail.com>
2024-06-20 08:12:37 +0530

    Add the Advance Qt file and modify Program.

--- Qobject_Study/main.cpp
+++ Qobject_Study/main.cpp
@@ -21,13 +21,51 @@ void viewQstring(){
 }
 //used to save the data in setting
 void saveSetting (QSettings &obj){
-    obj.setValue("name","I am vvv");
+    obj.beginGroup("Hello");
+    obj.setValue("Key1","value1");
+    obj.setValue("key2",1);
+    obj.endGroup();
 }
 //used to view the data in setting
 void viewSetting(QSettings &obj){
-    qInfo()<< obj.fileName();
     QVariant val = obj.value("name",QVariant());
     qInfo()<<"Return Value = "<< val.toString();
+
+    obj.beginGroup("Hello");
+    int val2 = obj.value("key2").toInt();
+    qInfo()<<"Return Value = "<< val2;
+    obj.endGroup();
+}
+
+void saveAge(QSettings *setting,QString group,QString name, int age)
+{
+    setting->beginGroup(group);
+    setting->setValue(name,age);
+    setting->endGroup();
+}
+
+int getAge(QSettings *setting, QString group, QString name)
+{
+    setting->beginGroup(group);
+
+    if(!setting->contains(name))
+    {
+        qWarning() << "Does not contain " << name << " in " << group;
+        setting->endGroup();
+        return 0;
+    }
+
+    bool ok;
+    int value = setting->value(name).toInt(&ok);
+    setting->endGroup();
+
+    if(!ok)
+    {
+        qWarning() << "Could not convert to int";
+        return 0;
+    }
+
+    return value;
 }
 
 
@@ -39,7 +77,7 @@ int main(int argc, char *argv[])
     viewQstring();
     *************/
 
-    /******QSetting Example
+    //******QSetting Example
     QCoreApplication::setOrganizationName("vvv_Organization");
     QCoreApplication::setOrganizationDomain("VVV_domine");
     QCoreApplication::setApplicationName("Qobject_study");
@@ -49,11 +87,17 @@ int main(int argc, char *argv[])
     if(setting.allKeys().isEmpty()){
         qInfo()<<"It is empty";
         saveSetting(setting);
+        saveAge(&setting,"people","Bryan",44);
+        saveAge(&setting,"beer","twoheart",1);
+        saveAge(&setting,"beer","Bryan",11);
     } else {
         qInfo()<<"It is not empty";
         viewSetting(setting);
+        qInfo() << "TwoHeart" << getAge(&setting,"beer","twoheart");
+        qInfo() << "Bryan (people)" << getAge(&setting,"people","Bryan");
+        qInfo() << "Bryan (beer)" << getAge(&setting,"beer","Bryan");
     }
-    **********************/
+    //**********************/
 
 
     return a.exec();
